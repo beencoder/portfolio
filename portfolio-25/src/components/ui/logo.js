9d1139fc7c9b01d91e3069ffd6077dceda410567
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-10-29 21:53:28 +0900

    refactor(logo): 이미지, 텍스트 구조로 전환 및 로고 이미지 교체

--- portfolio-25/src/components/ui/logo.js
+++ portfolio-25/src/components/ui/logo.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from '@/styles/components/logo.module.scss';
 
 export default function Logo() {
   return (
     <Link href="/" className={styles.logo}>
-      dakong.dev
+      <div className={styles['image-wrap']}>
+        <img src="/images/common/logo.png" alt="다빈의 포트폴리오 사이트 로고" />
+      </div>
+      Dabeen
     </Link>
   );
 }
