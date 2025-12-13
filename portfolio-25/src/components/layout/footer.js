5fbf06170ca2b0f6d97e2b118586926623112638
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-12-13 19:05:15 +0900

    feat(footer): footer 마크업 및 스타일링 추가

--- portfolio-25/src/components/layout/footer.js
+++ portfolio-25/src/components/layout/footer.js
@@ -1,9 +1,14 @@
+import clsx from 'clsx';
+
 import styles from '@/styles/layout/footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={styles.footer} role="contentinfo">
-      <div className="container">© {new Date().getFullYear()} dakong</div>
+      <div className={clsx('container', styles.layout)}>
+        <p>© {new Date().getFullYear()} Dabeen. All rights reserved.</p>
+        <p className={styles.description}>Designed & Developed by Dabeen</p>
+      </div>
     </footer>
   );
 }
